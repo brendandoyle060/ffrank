a58a5aea9c79374fccbd92f5f740c6ba11e81ae7
Brendan Doyle <bdoyle@ccs.neu.edu>
2015-05-01 09:40:18 -0400

    test

--- server.js
+++ server.js
@@ -160,7 +160,7 @@ app.post("/rest/user", auth, function(req, res){
             });
         }
     });
-});
+}); 
 
 var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
