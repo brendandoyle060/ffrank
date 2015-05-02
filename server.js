4cc5c4ac965f67d8df85c4119587ccec59dc51c7
Brendan Doyle <bdoyle@ccs.neu.edu>
2015-05-02 00:46:51 -0400

    mongo

--- server.js
+++ server.js
@@ -10,6 +10,18 @@ var multer     = require('multer');
 var cookieParser = require('cookie-parser');
 var session      = require('express-session');
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var WebSiteSchema = new mongoose.Schema({
+    name: String,
+    created: {type: Date, default: Date.now}
+}, {collection: 'website'});
+
+var WebSiteModel = mongoose.model('WebSite', WebSiteSchema);
+
+
+
 app.use(cookieParser())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,6 +31,25 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(express.static(__dirname + '/public'));
+
+app.get('/api/website/:name/create', function (req, res) {
+    var website = new WebSiteModel({ name: req.params.name });
+    website.save(function(err, doc) {
+        res.json(doc);
+    });
+});
+
+app.get('/api/website/:id', function (req, res) {
+    WebSiteModel.findById(req.params.id, function (err, site) {
+        res.json(site);
+    });
+});
+
+app.get('/api/website', function (req, res) {
+    WebSiteModel.find(function (err, sites) {
+        res.json(sites);
+    });
+});
 
 passport.use(new LocalStrategy(
 function(username, password, done)
