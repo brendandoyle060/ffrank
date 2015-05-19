3cc1121fb3657145337dc2a815858ed24bb9e6c7
Brendan Doyle <bdoyle@ccs.neu.edu>
2015-05-19 17:23:53 -0400

    Fixed XML for all positions

--- public/views/profile/profile.js
+++ public/views/profile/profile.js
@@ -87,45 +87,56 @@ app.controller('FFApiCtrl', function($scope, $http){
 app.controller('QbCtrl', function($scope, $http){
     console.log("QbCtrl entered");
 
-    var xmlhttp = new XMLHttpRequest();
+    var positions = ["qb", "rb", "wr", "te", "def", "k"];
+    var arraysArray = [];
 
-    // var filename = "../xml/" + position + ".xml";
-    var filename = "xml/qb.xml";
+    for (x = 0; x < positions.length; x++) {
 
-    xmlhttp.open("GET", filename, false);
-    xmlhttp.send();
-    console.log("XMLHTTP");
-    console.log(xmlhttp);
-    console.log(typeof xmlhttp);
-    var xmlDoc = xmlhttp.responseXML; 
-    console.log("XMLDOC");
-    console.log(xmlDoc);
-    console.log(typeof xmlDoc);
+        var xmlhttp = new XMLHttpRequest();
 
-    var allPlayerObjects = xmlDoc.getElementsByTagName("Player");
-    var allPlayerNames = [];
-    var name = "";
+        var filename = "../xml/" + positions[x] + ".xml";
+        // var filename = "xml/qb.xml";
 
-    for (i = 0; i < allPlayerObjects.length; i++) { 
-        name = allPlayerObjects[i].getAttribute("displayName");
-        console.log("player: " + name);
-        allPlayerNames.push(name);
+        xmlhttp.open("GET", filename, false);
+        xmlhttp.send();
+        console.log("XMLHTTP");
+        console.log(xmlhttp);
+        console.log(typeof xmlhttp);
+        var xmlDoc = xmlhttp.responseXML; 
+        console.log("XMLDOC");
+        console.log(xmlDoc);
+        console.log(typeof xmlDoc);
+
+        var allPlayerObjects = xmlDoc.getElementsByTagName("Player");
+        var allPlayerNames = [];
+        var name = "";
+
+        for (i = 0; i < allPlayerObjects.length; i++) { 
+            name = allPlayerObjects[i].getAttribute("displayName");
+            console.log("player: " + name);
+            allPlayerNames.push(name);
+        }
+
+        arraysArray.push(allPlayerNames);
+
     }
 
-    // $scope.qbList = allPlayerNames;
-
-
-    $scope.allQBs = write_letters(allPlayerNames);
-
+    $scope.qb = write_letters("qb", arraysArray[0]);
+    $scope.rb = write_letters("rb", arraysArray[1]);
+    $scope.wr = write_letters("wr", arraysArray[2]);
+    $scope.te = write_letters("te", arraysArray[3]);
+    $scope.def = write_letters("def", arraysArray[4]);
+    $scope.k = write_letters("k", arraysArray[5]);
 
 });
 
 
-function write_letters(listOfNames){
-    var items = document.getElementById("qbUl");
+function write_letters(pos, listOfNames){
+    var items = document.getElementById(pos);
 
     for (var i = 0; i < listOfNames.length; i++) {
         var item = document.createElement("li");
+        item.setAttribute("class", "ui-state-default");
         item.innerHTML = "<span class=\"ui-icon ui-icon-arrowthick-2-n-s\"></span>" + listOfNames[i];
         items.appendChild(item);
     }
