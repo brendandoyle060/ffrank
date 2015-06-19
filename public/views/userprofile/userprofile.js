6b07cef6187e59d3241155301d0bf4e96285dea6
Brendan Doyle <bdoyle@ccs.neu.edu>
2015-06-19 16:52:39 -0400

    Removed inconvenient footers from all pages, and added ability to remove favorites from that user's page. Next step is adding a button to the main profile page's favorites list that will allow users to be removed from there.

--- public/views/userprofile/userprofile.js
+++ public/views/userprofile/userprofile.js
@@ -29,23 +29,69 @@ app.controller("UserCtrl", function($scope, $http, $location, $rootScope, $route
         });
     };
 
+    $scope.isFavorite = function(uName) {
+        return isInFavorites(uName);
+    };
+
+    var isInFavorites = function(uName) {
+        console.log("isInFavorites uName: " + uName);
+
+        var currentFavorites = $rootScope.currentUser.favorites;
+        console.log("currentFavorites in isInFavorites: " + currentFavorites);
+        if ($.inArray(uName, currentFavorites) > -1) {
+            console.log("isInFavorites returning true");
+            return true;
+        } else {
+            console.log("isInFavorites returning false");
+            return false;
+        }
+
+    };
+
 
     $scope.addToFavorites = function(uName) {
-        // console.log("addToFavorites uName: " + uName);
+        console.log("addToFavorites uName: " + uName);
 
-        var existingFavorites = $rootScope.currentUser.favorites;
-        console.log("existingFavorites before: " + existingFavorites);
-        existingFavorites.push(uName);
-        console.log("existingFavorites after: " + existingFavorites);
+        var currentFavorites = $rootScope.currentUser.favorites;
+        console.log("currentFavorites before addToFavorites: " + currentFavorites);
+        if (!isInFavorites(uName)) {
+            currentFavorites.push(uName);
+        }
+        console.log("currentFavorites after addToFavorites: " + currentFavorites);
 
 
         console.log("$rootScope.currentUser.favorites: " + $rootScope.currentUser.favorites);
-        $rootScope.currentUser.favorites = existingFavorites;
+        $rootScope.currentUser.favorites = currentFavorites;
         console.log("$rootScope.currentUser.favorites: " + $rootScope.currentUser.favorites);
 
         $http.put("/usermodels/favorites/" + $rootScope.currentUser.username, $rootScope.currentUser)
         .success(function(response){
             console.log("PUT ADDTOFAVORITES CALLED");
+            console.log("uName: " + uName);
+            console.log("response: " + response);
+            
+        });
+    };
+
+    $scope.removeFromFavorites = function(uName) {
+        console.log("removeFromFavorites uName: " + uName);
+
+        var currentFavorites = $rootScope.currentUser.favorites;
+        console.log("currentFavorites before removeFromFavorites: " + currentFavorites);
+        var removeIndex = currentFavorites.indexOf(uName);
+        if (removeIndex > -1) {
+            currentFavorites.splice(removeIndex, 1);
+        }
+        console.log("currentFavorites after removeFromFavorites: " + currentFavorites);
+
+
+        console.log("$rootScope.currentUser.favorites: " + $rootScope.currentUser.favorites);
+        $rootScope.currentUser.favorites = currentFavorites;
+        console.log("$rootScope.currentUser.favorites: " + $rootScope.currentUser.favorites);
+
+        $http.put("/usermodels/favorites/" + $rootScope.currentUser.username, $rootScope.currentUser)
+        .success(function(response){
+            console.log("PUT REMOVEFROMFAVORITES CALLED");
             console.log("uName: " + uName);
             console.log("response: " + response);
             
